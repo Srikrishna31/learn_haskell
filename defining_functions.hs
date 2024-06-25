7a8878dc37814fca91dc2dd0c1683ccd02a0c549
Krishna Addepalli <coolkrishna31@gmail.com>
2024-06-25 08:39:03 +0200

    Chapter 4: Solutions to exercises

--- defining_functions.hs
+++ defining_functions.hs
@@ -1,4 +1,3 @@
-import qualified Control.Applicative as Haskell
 -- Defining functions
 
 {-
@@ -75,9 +74,35 @@ same type. If the first pattern is matched, then the first result is
 chosen; otherwise if the second is matched, then the second result is 
 chosen, and so on.
 -}
-(&&) :: Bool -> Bool -> Bool
-True && True = True
-_ && _ = False
+and :: Bool -> Bool -> Bool
+True `and` True = True
+_ `and` _ = False
+
+and' :: Bool -> Bool -> Bool
+True `and'` True = True
+_ `and'` _ = False
+
+and'' :: Bool -> Bool -> Bool
+True `and''` b = b
+False `and''` _ = False
+
+and''' :: Bool -> Bool -> Bool
+b `and'''` c | b == c = b
+             | otherwise = False
+
+and'''' :: Bool -> Bool -> Bool
+b `and''''` c = if b then 
+                    if c then 
+                        True 
+                    else 
+                        False 
+                else False
+
+
+and''''' :: Bool -> Bool -> Bool
+True `and'''''` b = if b then True else False
+False `and'''''` _ = False
+
 
 {-
 Tuple Patterns
@@ -233,3 +258,130 @@ arguments to other functions.
 -}
 sum :: [Int] -> Int
 sum = foldl (+) 0
+
+
+-- Exercises
+
+{-
+Using library functions, define a function halve, that splits an 
+even-lengthed list into two halves.
+-}
+halve ::[a] -> ([a], [a])
+-- halve xs = (take l xs, drop l xs)
+--             where l = length xs
+halve xs | Main.even (len) = (take (h) xs, drop (h) xs)
+                                where len = length xs; h = len `div` 2
+
+
+{-
+Define a function third::[a] -> a that returns the third element in a 
+list that contains at least this many elements using:
+a. head and tail;
+b. list indexing !!
+c. pattern matching
+-}
+third:: [a] -> a
+third xs = Prelude.head (Prelude.tail (Prelude.tail xs))
+
+third'' :: [a] -> a
+third'' xs = xs !! 2
+
+third' :: [a] -> a
+third' (_:_:x:_) = x
+
+
+{-
+Consider a function safetail :: [a] -> [a] that behaves in the same way
+as tail except that it maps the empty list to itself rather than producing
+an error. Using tail and the function null :: [a] -> Bool that decides if
+a list is empty or not, define safetail using:
+
+a. a conditional expression;
+
+b. guarded equations;
+
+c. pattern matching
+-}
+safetail :: [a] -> [a]
+safetail xs = if null xs then [] else Prelude.tail xs
+
+safetail' :: [a] -> [a]
+safetail' xs | null xs = []
+             | otherwise = Prelude.tail xs
+
+safetail'' :: [a] -> [a]
+safetail'' (_:xs) = xs
+safetail'' _ = []
+
+
+{-
+In a similar way to && in section 4.4, show how the disjunction operator ||
+can be defined in four different ways using pattern matching
+-}
+or :: Bool -> Bool -> Bool
+True `or` True = True
+True `or` False = True
+False `or` True = True
+False `or` False = False
+
+or' :: Bool -> Bool -> Bool
+False `or'` False = False
+_ `or'` _ = True
+
+or'' :: Bool -> Bool -> Bool
+b `or''` c | b == c && not b = False
+           | otherwise = True
+
+or''' :: Bool -> Bool -> Bool
+False `or'''` b = b
+True `or'''` _ = True
+
+
+
+{-
+Show how the meaning of the following curried function definition can
+be formalized in terms of lambda expressions:
+-}
+mult :: Int -> Int -> Int -> Int
+mult x y z = x * y * z
+
+
+mult':: Int -> Int -> Int -> Int
+mult' = \x -> \y -> \z -> x * y * z
+
+
+{-
+The Luhn algorithm is used to check bank card numbers for simple
+errors such as mistyping a digit, and proceeds as follows:
+
+* consider each digit as a separate number;
+* moving left, double every other number from the second last;
+* subtract 9 from each number that is now greater than 9;
+* add all the resulting numbers together;
+* if the total is divisible by 10, the card number is valid.
+
+Define a function luhnDouble :: Int -> Int that doubles a digit
+and subtracts 9 if the result is greater than 9. For example:
+    > luhnDouble 3
+    6
+
+    > luhnDouble 6
+    3
+
+Using luhnDouble and the integer remainder function mod, define
+a function luhn:: Int -> Int -> Int -> Int -> Bool that decides
+if a four-digit bank card number is valid. For example:
+    > luhn 1 7 8 4
+    True
+
+    > luhn 4 7 8 3
+    False
+
+In the exercises for chapter 7 we will consider a more general
+version of this function that accepts card numbers of any length.
+-}
+luhnDouble :: Int -> Int
+luhnDouble b = if 2*b < 9 then 2*b else 2*b - 9
+
+luhn :: Int -> Int -> Int -> Int -> Bool
+luhn a b c d = (luhnDouble a +  b + luhnDouble c + d) `mod` 10 == 0
