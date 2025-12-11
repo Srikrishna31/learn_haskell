31d6377834bd1c18899a3bcceb559079c12e1c70
Krishna Addepalli <coolkrishna31@gmail.com>
2025-12-11 15:59:06 +0100

    Add solution to the DepthFirst search problem

--- haskell_from_zero_to_expert/graphs.hs
+++ haskell_from_zero_to_expert/graphs.hs
@@ -21,18 +21,37 @@ Write a function acyclicPaths :: Eq a => a -> a -> [(a,a)] -> [[a]] that given t
 
 In graph theory, a cycle in a graph is a non-empty trail in which only the first and last vertices are equal.
 -}
-acyclicPaths :: Eq a => a -> a -> [(a,a)] -> [[a]]
+acyclicPaths :: (Eq a) => a -> a -> [(a, a)] -> [[a]]
 acyclicPaths source sink edges
-    | source == sink = [[sink]]
-    | otherwise = [
-        source:path | edge <- edges, (fst edge) == source,
-            path <- (acyclicPaths (snd edge) sink [e | e <- edges, e/=edge])
-    ]
-
+  | source == sink = [[sink]]
+  | otherwise =
+      [ source : path
+      | edge <- edges,
+        (fst edge) == source,
+        path <- (acyclicPaths (snd edge) sink [e | e <- edges, e /= edge])
+      ]
 
 -- Write a function depthFisrt :: Graph -> Node -> [Node] that generates a depth-first order graph traversal sequence Use the following graph notation:
+{-
+The algorithm starts at a starting node and explores as far as possible along each branch before backtracking
+-}
 type Node = Int
+
 type Edge = (Node, Node)
+
 type Graph = ([Node], [Edge])
 
 depthFirst :: Graph -> Node -> [Node]
+depthFirst (v, e) node
+  | null [x | x <- v, x == node] = [] -- if the set of nodes is empty, return empty list.
+  | otherwise = depthrec (v, e) [node]
+  where
+    depthrec :: Graph -> [Node] -> [Node]
+    depthrec ([], _) _ = []
+    depthrec (_, _) [] = []
+    depthrec (v, e) (top : stack)
+      | null [x | x <- v, x == top] = depthrec (newv, e) stack
+      | otherwise = top : depthrec (newv, e) (adjacent ++ stack)
+      where
+        adjacent = [x | (x, y) <- e, y == top] ++ [x | (y, x) <- e, y == top]
+        newv = [x | x <- v, x /= top]
